ff8a843a577f7fe1c4007694330d568a702df201
Aditya Rajput <rajaditya3998@gmail.com>
2019-09-06 08:30:40 +0530

    Added Links, added images, updated contacts

---
+++ referrals/index.js
@@ -0,0 +1,27 @@
+const url_string = window.location.href
+const url = new URL(url_string)
+
+const firstname = url.searchParams.get("firstname")
+const lastname = url.searchParams.get("lastname")
+const college = url.searchParams.get("college")
+const year = url.searchParams.get("year")
+const branch = url.searchParams.get("branch")
+const phone = url.searchParams.get("phone")
+
+// api_url = 'http://localhost:5000/add?'
+api_url = 'https://tusharsadhwani1.pythonanywhere.com/add?'
+
+if (firstname && lastname && college && year && branch && phone) {
+    window.history.replaceState(null, null, window.location.pathname);
+    document.getElementById("form").reset()
+
+    messageBox = document.getElementById("message")
+    messageBox.textContent = "Processing..."
+
+    fetch(api_url + url_string.split('?')[1])
+    .then(res => res.json())
+    .then(body => {
+        messageBox.style.backgroundColor = body.success ? 'rgb(31, 177, 87)' : 'red'
+        messageBox.textContent = body.message
+    })
+}
