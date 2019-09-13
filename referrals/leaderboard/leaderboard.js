a5d13323ab1aa389ab0b40aaa0df8cf43372eb22
Tushar Sadhwani <18bit160@ietdavv.edu.in>
2019-09-13 23:31:24 +0530

    added check your points

--- referrals/leaderboard/leaderboard.js
+++ referrals/leaderboard/leaderboard.js
@@ -3,6 +3,29 @@ vm = new Vue({
   data: {
     cards: [],
     iet_cards: []
+  },
+  methods: {
+    checkpoints: function () {
+      ref_field = document.getElementById("check_ref")
+      points_field = document.getElementById("check_points")
+      ref_field.textContent = ""
+      points_field.textContent = ""
+
+      ref_id = document.getElementById("ref-id").value
+      if (ref_id) {
+        fetch('https://tusharsadhwani1.pythonanywhere.com/check?ref='+ref_id)
+          .then(res => res.json())
+          .then(data => {
+            if (data.success) {
+              ref_field.textContent = data.result.referral
+              points_field.textContent = data.result.count
+            } else {
+              ref_field.textContent = ref_id
+              points_field.textContent = "Not found"
+            }
+          })
+      }
+    }
   }
 })
 
